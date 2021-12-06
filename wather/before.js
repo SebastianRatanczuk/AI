cbaee7eeafc9e65eb24262d5933324beba6ca45d
Sebastian Ratańczuk <46678414+Ictis0@users.noreply.github.com>
2021-12-06 12:13:29 +0100

    123

--- wather/before.js
+++ wather/before.js
@@ -2,23 +2,76 @@ const WeatherApp = class {
     constructor(apiKey, resultsBlockSelector) {
         this.apiKey = apiKey;
         this.resultsBlock = document.querySelector(resultsBlockSelector)
+        this.currentWatherLink = `https://api.openweathermap.org/data/2.5/weather?q={query}&appid=${apiKey}&units=metric&lang=pl`;
+        this.currentWeather = undefined;
+
+        this.forecastLink = `https://api.openweathermap.org/data/2.5/forecast?q={query}&appid=${apiKey}&units=metric&lang=pl`;
+        this.forecast = undefined;
+
+
     }
 
     getCurrentWeather(query) {
+        let url = this.currentWatherLink.replace("{query}", query);
 
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.currentWeather = data;
+                this.drawWeather();
+            });
     }
 
     getForecast(query) {
+        let url = this.forecastLink.replace("{query}", query);
 
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.forecast = data.list;
+                this.drawWeather();
+            });
     }
 
     getWeather(query) {
-        const weatherBlock = this.createWeatherBlock('2020-12-11 17:30', 21.21, 23.4, '04n', 'scattered clouds')
-        this.resultsBlock.appendChild(weatherBlock);
+        this.getCurrentWeather(query);
+        this.getForecast(query);
     }
 
     drawWeather() {
 
+        this.resultsBlock.innerHTML = "";
+        if (this.currentWeather) {
+            const date = new Date(this.currentWeather.dt * 1000);
+            const weatherBlock = this.createWeatherBlock(
+                `${date.toLocaleDateString("pl-PL")} ${date.toLocaleTimeString("pl-PL")}`,
+                this.currentWeather.main.temp,
+                this.currentWeather.main.feels_like,
+                this.currentWeather.weather[0].icon,
+                this.currentWeather.weather[0].description
+            );
+            this.resultsBlock.appendChild(weatherBlock);
+        }
+
+        if (this.forecast) {
+            for (let i = 0; i < this.forecast.length; i++) {
+                let weather = this.forecast[i];
+
+                const date = new Date(weather.dt * 1000);
+                const weatherBlock = this.createWeatherBlock(
+                    `${date.toLocaleDateString("pl-PL")} ${date.toLocaleTimeString("pl-PL")}`,
+                    weather.main.temp,
+                    weather.main.feels_like,
+                    weather.weather[0].icon,
+                    weather.weather[0].description
+                );
+                this.resultsBlock.appendChild(weatherBlock);
+            }
+        }
     }
 
     createWeatherBlock(dateString, temperature, feelsLikeTemperature, iconName, description) {
